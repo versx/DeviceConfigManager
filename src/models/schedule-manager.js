dd242eead0503d4f5910f84c671e234dee6f3e91
versx <versefx@gmail.com>
2020-04-30 19:15:00 -0700

    Add device notes column. Fix scheduler for multiple devices.

--- src/models/schedule-manager.js
+++ src/models/schedule-manager.js
@@ -111,9 +111,8 @@ class ScheduleManager {
     }
     static async triggerSchedule(schedule, config) {
         console.log('Running schedule for', schedule, 'to assign config', config);
-        var uuids = schedule.uuids.split(',');
-        if (uuids) {
-            uuids.forEach(async function(uuid) {
+        if (schedule.uuids) {
+            schedule.uuids.forEach(async function(uuid) {
                 var device = await Device.getByName(uuid);
                 // Check if the device config is not already set to the scheduled config to assign.
                 if (device.config !== config) {
