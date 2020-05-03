31e0cba0f4c54a6470bba614df1066391f9f90ae
Leevo <root@localhost.localdomain>
2020-05-03 02:46:48 +0200

    local changes

--- src/routes/api.js
+++ src/routes/api.js
@@ -106,7 +106,7 @@ router.get('/devices', async function(req, res) {
             var delta = 15 * 60;
             var isOffline = device.last_seen > (Math.round((new Date()).getTime() / 1000) - delta) ? 0 : 1;
             var image = exists ? `/screenshots/${device.uuid}.png` : (isOffline ? '/img/offline.png' : '/img/online.png');
-            device.image = `<a href='${image}' target='_blank'><img src='${image}' width='72' height='96'/></a>`;
+            device.image = `<a href='${image}' target='_blank'><img src='${image}' width='96' height='96' style='margin-left: auto;margin-right: auto;display: block;'/></a>`;
             device.last_seen = utils.getDateTime(device.last_seen);
             device.buttons = `
             <div class='btn-group' role='group'>
