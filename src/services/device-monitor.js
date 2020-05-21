10c57f5f78b790f0788042628e666e111654b51a
versx <versefx@gmail.com>
2020-05-21 15:55:34 -0700

    Add device check interval to config

--- src/services/device-monitor.js
+++ src/services/device-monitor.js
@@ -6,7 +6,7 @@ const logger = require('./logger.js');
 const config = require('../config.json');
 const Device = require('../models/device.js');
 const utils = require('../utils.js');
-const devicesCheckInterval = 5 * 60 * 1000; // Check every 5 minutes
+const devicesCheckInterval = (config.monitor.interval || 5) * 60 * 1000; // Check every 5 minutes
 const delta = 15 * 60;
 let lastUpdate = -2;
 
