61282cd677178371c98cb267da5caae79abd64fe
versx <versx@ver.sx>
2020-06-29 19:17:08 -0700

    Increase uuid_date_type column length

--- migrations/10.sql
+++ migrations/10.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS `stats` (
   `uuid_date_type` VARCHAR(45) NOT NULL,
   `counter` INT(15) NOT NULL,
-  PRIMARY KEY (`uuid_date_type`));
+  PRIMARY KEY (`uuid_date_type`)
+);
