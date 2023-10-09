7dcc0839403745813956bc90fd84d6804373f35f
versx <versx@ver.sx>
2023-10-08 18:39:51 -0700

    replace pino with tslog and add log rotator

--- server/src/services/LogService.ts
+++ server/src/services/LogService.ts
@@ -1,7 +1,8 @@
 import { resolve } from 'path';
-import pino, { Logger } from 'pino';
+import { Logger } from 'tslog';
+import { createStream } from 'rotating-file-stream';
 
-const loggers: { [name: string]: Logger } = {};
+const loggers: { [name: string]: Logger<any> } = {};
 
 const getLoggers = () => loggers;
 
@@ -10,36 +11,33 @@ const getLogger = (name: string, level: string = 'info') => {
     return loggers[name];
   }
 
-  const logPath = resolve(`./static/logs/${name}.log`);
-  return loggers[name] = pino({
-    name,
-    level,
-    timestamp: true,
-    enabled: true,
-    //redact: {
-    //  paths: ['hostname'],
-    //  censor: ''
-    //},
-    base: undefined,
-    transport: {
-      targets: [{
-        target: 'pino-pretty',
-        level,
-        options: {
-          colorize: true,
-        },
-      },{
-        target: 'pino/file',
-        level,
-        options: {
-          destination: logPath,
-          //minLength: 4096, // Buffer before writing
-          append: true,
-          sync: false,
-        },
-      }],
-    },
+  const logFolder = resolve(__dirname, '../../static/logs');
+  const logFileName = `${name}.log`;
+  //const logPath = resolve(logFolder, logFileName);
+  const stream = createStream(logFileName, {
+    size: '1M', // rotate every 10 MegaBytes written
+    maxSize: '1M', // rotate every 10 MegaBytes written
+    interval: '1d', //'1h', // rotate daily
+    compress: 'gzip', // compress rotated files
+    path: logFolder,
   });
+  stream.on('rotated', (filename) => {
+    console.log('Log file rotated:', filename);
+  });
+  stream.on('error', (err) => {
+    console.error('Log rotation error:', err);
+  });
+
+  const logger = new Logger({
+    name,
+    //minLevel: level,
+    type: 'pretty',
+  });
+  logger.attachTransport((logObj) => {
+    stream.write(JSON.stringify(logObj) + '\n');
+  });
+
+  return loggers[name] = logger;
 };
 
 const deleteLogger = (name: string) => {
