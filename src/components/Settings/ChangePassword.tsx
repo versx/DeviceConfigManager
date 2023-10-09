c992552e99b383713497ca96e504af52f37e6526
versx <versx@ver.sx>
2023-10-09 08:26:41 -0700

    fix issue with change password component error helpers

--- src/components/Settings/ChangePassword.tsx
+++ src/components/Settings/ChangePassword.tsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-import {
-  Button,
-  TextField,
-} from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { useSnackbar } from 'notistack';
 
 import { AuthService, UserService } from '../../services';
@@ -12,18 +9,46 @@ export const ChangePassword = () => {
   const [currentPassword, setCurrentPassword] = useState<string>('');
   const [newPassword, setNewPassword] = useState<string>('');
   const [confirmPassword, setConfirmPassword] = useState<string>('');
-  const [error, setError] = useState<string | null>(null);
   const { enqueueSnackbar } = useSnackbar();
   const currentUser = getUserToken();
 
+  const [errors, setErrors] = useState({
+    currentPassword: false,
+    newPassword: false,
+    newPasswordConfirm: false,
+  });
+
   const handleSubmit = async () => {
-    if (newPassword !== confirmPassword) {
-      setError('New password and confirmation password do not match');
+    if (!currentPassword) {
+      setErrors({...errors, currentPassword: true});
       return;
+    } else {
+      setErrors({...errors, currentPassword: false});
+    }
+
+    if (!newPassword) {
+      setErrors({...errors, newPassword: true});
+      return;
+    } else {
+      setErrors({...errors, newPassword: false});
+    }
+
+    if (!confirmPassword) {
+      setErrors({...errors, newPasswordConfirm: true});
+      return;
+    } else {
+      setErrors({...errors, newPasswordConfirm: false});
+    }
+
+    if (newPassword !== confirmPassword) {
+      setErrors({...errors, newPassword: true, newPasswordConfirm: true});
+      return;
+    } else {
+      setErrors({...errors, newPassword: false, newPasswordConfirm: false});
     }
 
     const response = await UserService.changePassword(currentUser?.id, currentPassword, confirmPassword);
-    if (response.status !== 'ok') {
+    if (response?.status !== 'ok') {
       enqueueSnackbar('Failed to change the password to your user account.', { variant: 'error' });
       return;
     }
@@ -36,31 +61,38 @@ export const ChangePassword = () => {
     <>
       <TextField
         fullWidth
+        required
         type="password"
         label="Current Password"
         variant="outlined"
         value={currentPassword}
+        error={!currentPassword && errors.currentPassword}
+        helperText={!currentPassword && errors.currentPassword ? 'Current password field is required.' : ''}
         onChange={e => setCurrentPassword(e.target.value)}
         style={{ marginBottom: '15px' }}
       />
       <TextField
         fullWidth
+        required
         type="password"
         label="New Password"
         variant="outlined"
         value={newPassword}
+        error={!newPassword && errors.newPassword}
+        helperText={!newPassword && errors.newPassword ? 'New password field is required.' : ''}
         onChange={e => setNewPassword(e.target.value)}
         style={{ marginBottom: '15px' }}
       />
       <TextField
         fullWidth
+        required
         type="password"
         label="Confirm New Password"
         variant="outlined"
         value={confirmPassword}
+        error={!confirmPassword && errors.newPasswordConfirm}
+        helperText={!confirmPassword && errors.newPasswordConfirm ? 'Confirm new password field is required.' : ''}
         onChange={e => setConfirmPassword(e.target.value)}
-        error={Boolean(error)}
-        helperText={error}
         style={{ marginBottom: '20px' }}
       />
       <Button
