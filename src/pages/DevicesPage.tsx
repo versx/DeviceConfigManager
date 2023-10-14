f84d968e8a0db4f59a7c4f9c9032148bc82f6d55
versx <versx@ver.sx>
2023-10-13 20:01:29 -0700

    add device restarts chart

--- src/pages/DevicesPage.tsx
+++ src/pages/DevicesPage.tsx
@@ -2,19 +2,45 @@ import React, { useCallback, useEffect, useState } from 'react';
 import {
   Button,
   Container,
+  TextField,
   Typography,
 } from '@mui/material';
 import { useSnackbar } from 'notistack';
+import {
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  Chart as ChartJS,
+  Legend,
+  LinearScale,
+  Title,
+  Tooltip,
+} from 'chart.js';
 
-import { DeviceGrid } from '../components';
+import { BarChart, DeviceGrid, DeviceStats } from '../components';
 import { ActiveMenuItemColor } from '../consts';
+import { formatDate } from '../modules';
 import { ConfigService, DeviceService } from '../services';
-import { Config, Device } from '../types';
+import { Config, Device, DeviceStat } from '../types';
 
 export const DevicesPage = () => {
   const [devices, setDevices] = useState<Device[]>([]);
   const [configs, setConfigs] = useState<Config[]>([]);
+  const [deviceStats, setDeviceStats] = useState<DeviceStat[]>([]);
+  const [date, setDate] = useState('');
   const { enqueueSnackbar } = useSnackbar();
+
+  ChartJS.register(ArcElement, BarElement, CategoryScale, Legend, LinearScale, Title, Tooltip);
+
+  const handleReloadStats = useCallback((uuid?: string, date?: string) => {
+    DeviceService.getDeviceStats(uuid ?? '', date ?? '').then((response: any) => {
+      if (response?.status !== 'ok') {
+        enqueueSnackbar(`Failed to fetch device stats with error: ${response?.error}`, { variant: 'error' });
+        return;
+      }
+      setDeviceStats(response.stats);
+    });
+  }, [enqueueSnackbar]);
 
   const handleReload = useCallback(() => {
     DeviceService.getDevices().then((response: any) => {
@@ -24,6 +50,7 @@ export const DevicesPage = () => {
       }
       setDevices(response.devices);
     });
+
     ConfigService.getConfigs().then((response: any) => {
       if (response?.status !== 'ok') {
         enqueueSnackbar(`Failed to fetch configs with error: ${response?.error}`, { variant: 'error' });
@@ -31,7 +58,14 @@ export const DevicesPage = () => {
       }
       setConfigs(response.configs);
     });
-  }, [enqueueSnackbar]);
+
+    handleReloadStats();
+  }, [enqueueSnackbar, handleReloadStats]);
+
+  const handleDateChange = (date: string) => {
+    setDate(date);
+    handleReloadStats('', date);
+  };
 
   useEffect(() => handleReload(), [handleReload]);
 
@@ -63,6 +97,29 @@ export const DevicesPage = () => {
         configs={configs}
         devices={devices}
         onReload={handleReload}
+      />
+
+      <TextField
+        fullWidth
+        margin="normal"
+        label="Date"
+        type="date"
+        value={date ? formatDate(new Date(date)) : ''}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        onChange={e => handleDateChange(e.target.value)}
+        style={{
+          marginBottom: 10,
+        }}
+      />
+
+      <BarChart
+        stats={deviceStats}
+      />
+
+      <DeviceStats
+        stats={deviceStats}
       />
     </Container>
   );
